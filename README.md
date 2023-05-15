134a755449cda564c0eec50ca9e7217cc3946d23
AoDev <aodev@AoDevs-MacBook-Pro.local>
2023-05-15 13:21:51 +0200

    feat: add config options for tag and svg-sprite

--- README.md
+++ README.md
@@ -1,21 +1,18 @@
 # vite-plugin-svg-spriter
 
-_Currently work in progress._
-
 Build a svg sprite from individual svgs and inject it in your root html with vite.
+
+Uses [svg-sprite](https://github.com/svg-sprite/svg-sprite) under the hood.
 
 ### Install
 
 `npm install vite-plugin-svg-spriter -DE`
 
-Careful, it's spriter, with an 'r'.
+Careful, it's `"spriter"`, with an `'r'`.
 
 ### Plugin usage
 
-In this initial version, the config is limited.
-You only need to pass the path to your svgs.
-
-Options will be added later.
+The plugin only requires the path to your svgs. Check the configuration section for options.
 
 ```ts
 // vite.config.ts
@@ -25,12 +22,14 @@ import createSvgSpritePlugin from 'vite-plugin-svg-spriter'
 import path from 'path'
 
 const SRC_PATH = path.resolve(__dirname, 'src')
-const SPRITE_PATH = path.resolve(SRC_PATH, 'assets', 'svg-sprite')
+const SVG_FOLDER_PATH = path.resolve(SRC_PATH, 'assets', 'svg-sprite')
 
 export default defineConfig({
-  plugins: [react(), createSvgSpritePlugin(SPRITE_PATH)],
+  plugins: [createSvgSpritePlugin({svgFolder: SVG_FOLDER_PATH})],
 })
 ```
+
+By default, the sprite is prepended to your root html body element and no transformation is applied to the svgs.
 
 ### Display svg
 
@@ -42,4 +41,28 @@ thumbs-up.svg
 <svg>
   <use xlinkHref="#thumbs-up" />
 </svg>
+```
+
+### Configuration (optional)
+
+- **svg-sprite**: You can customize the output by providing a [svg-sprite config](https://github.com/svg-sprite/svg-sprite#configuration-basics).
+
+- **injection**: You can provide a HtmlTagDescriptor for Vite [transformIndexHtml hook tag](https://vitejs.dev/guide/api-plugin.html#transformindexhtml)
+
+```ts
+defineConfig({
+  plugins: [
+    createSvgSpritePlugin({
+      svgFolder: SVG_FOLDER_PATH,
+      svgSpriteConfig: {
+        shape: {
+          transform: ['svgo'],
+        },
+      },
+      transformIndexHtmlTag: {
+        injectTo: 'body',
+      },
+    }),
+  ],
+})
 ```
--- src/createSVGSpritePlugin.ts
+++ src/createSVGSpritePlugin.ts
@@ -4,24 +4,40 @@ import SVGSprite from 'svg-sprite'
 import type {Config} from 'svg-sprite'
 import type {Plugin, HtmlTagDescriptor} from 'vite'
 
-const spriterConfig: Config = {
+interface IConfig {
+  svgFolder: string
+  svgSpriteConfig?: Config
+  transformIndexHtmlTag?: Partial<Omit<HtmlTagDescriptor, 'children'>>
+}
+
+const defaultSpriterConfig: Config = {
   // @ts-expect-error types definition have an issue https://github.com/DefinitelyTyped/DefinitelyTyped/discussions/65401
   mode: {symbol: {inline: true}},
-  shape: {transform: []}, // remove svgo
+  shape: {transform: []}, // remove default svgo
+}
+
+const defaultTag: HtmlTagDescriptor = {
+  tag: 'div',
+  attrs: {'aria-hidden': true},
+  injectTo: 'body-prepend',
 }
 
 let symbolList: string[] = []
 
-function createSVGSpritePlugin(folderPath: string): Plugin {
+function createSVGSpritePlugin({
+  svgFolder,
+  svgSpriteConfig = {},
+  transformIndexHtmlTag = {},
+}: IConfig): Plugin {
   return {
     name: 'svg-sprite',
     async transformIndexHtml() {
-      const files = fs.readdirSync(folderPath)
-      const spriter = new SVGSprite(spriterConfig)
+      const files = fs.readdirSync(svgFolder)
+      const spriter = new SVGSprite({...defaultSpriterConfig, ...svgSpriteConfig})
       symbolList = []
 
       for (const file of files) {
-        const filePath = path.resolve(folderPath, file)
+        const filePath = path.resolve(svgFolder, file)
         const svg = fs.readFileSync(filePath, 'utf-8')
         spriter.add(filePath, null, svg)
         const symbolName = file.replace('.svg', '')
@@ -31,10 +47,9 @@ function createSVGSpritePlugin(folderPath: string): Plugin {
       const {result} = await spriter.compileAsync()
       const sprite = result.symbol.sprite.contents.toString()
       const spriteTag: HtmlTagDescriptor = {
-        tag: 'div',
-        attrs: {'aria-hidden': true},
+        ...defaultTag,
+        ...transformIndexHtmlTag,
         children: sprite,
-        injectTo: 'body-prepend',
       }
 
       return [spriteTag]
